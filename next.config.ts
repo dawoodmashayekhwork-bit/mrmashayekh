fe2379ad25e2b1150c6e5aa10502e0fd6673ef0a
Dawood <dawoodmashayekh.work@gmail.com>
2026-08-06 15:29:29 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true, // برای اینکه تصاویر در گیت‌هاب پیجز بدون خطا لود شوند
+  },
 };
 
 export default nextConfig;
