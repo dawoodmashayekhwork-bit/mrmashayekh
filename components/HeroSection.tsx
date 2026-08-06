0651c3c51003baff03b09cfe65e85ed6196d8cca
Dawood <dawoodmashayekh.work@gmail.com>
2026-08-06 16:16:32 +0330

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -26,7 +26,7 @@ export default function HeroSection() {
           transition={{ duration: 1, delay: 2 }}
           className="text-accent text-xs md:text-sm tracking-[0.4em] uppercase mb-4 font-medium"
         >
-          استودیوی معماری و طراحی
+           معماری و طراحی
         </motion.span>
 
         <motion.h1
